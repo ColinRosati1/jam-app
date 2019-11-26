21b30689c6683656458d6b17d396c8983281ac1a
Colin Rosati <colin.james.rosati@gmail.com>
2019-11-26 19:33:57 +0100

    create jam feed component as class with state structure and api call on mount

--- src/components/JamFeed.jsx
+++ src/components/JamFeed.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-function JamFeed() {
-  return (
-    <div className="jam-feed">
-      feed
-    </div>
-  );
+
+class JamFeed extends React.Component {
+    constructor(){
+        super()
+        this.state = {results: []};
+    }
+    componentDidMount(){
+        const url = "https://api-stg.jam-community.com/song/trending";
+        fetch(url)
+        .then( async (response) => {
+            console.log(response)
+        })
+        .then( async results => { 
+            await this.setState({results}) 
+        })
+        .catch( err => console.log("error with user fetch data"))
+        
+    }
+    render(){ // handle API data here. render each array object into DOM elements
+        let items = this.state.results; // grab state data
+        return(
+            <div className="jam-feed-wrapper">
+                {items}
+            </div>
+            
+        )
+    }
 }
 
 export default JamFeed;
